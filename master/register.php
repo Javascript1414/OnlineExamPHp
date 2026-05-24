034d050384194962c3b34669a37e513f60b03f25
Javascript1414 <soumosantra588@gmail.com>
2026-05-24 10:41:38 +0530

    first commit

---
+++ master/register.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Online Examination System in PHP</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
